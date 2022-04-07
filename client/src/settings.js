568ea907d3b3920bc9693c3439af9b6e8f95bda3
Albert Aghajanyan <albert.aghajanyan.mw@gmail.com>
2022-04-08 01:15:59 +0400

    Updated keys

--- client/src/settings.js
+++ client/src/settings.js
@@ -8,12 +8,12 @@ const SETTINGS = {
         publicVapid: process.env.REACT_APP_FIREBASE_PUBLIC_VAPID,
         configs: {
           apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-          authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-          projectId: process.env.REACT_APP_PROJECT_ID,
-          storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-          messagingSenderId: process.env.REACT_APP_MESSENGER_SENDER_ID,
-          appId: process.env.REACT_APP_APP_ID,
-          measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+          authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_SENDER_ID,
+          appId: process.env.REACT_APP_FIREBASE_APP_ID,
+          measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
         }
       }
 };
