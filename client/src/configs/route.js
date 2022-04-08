d6d87b2710113e3d773e2b4244d7ed22f6059196
albertaghajanyanmw <101137879+albertaghajanyanmw@users.noreply.github.com>
2022-04-08 20:38:19 +0400

    Commited Todo app (#4)

---
+++ client/src/configs/route.js
@@ -0,0 +1,31 @@
+const routes = {
+  home: {
+    path: '/',
+    title: 'pages.home'
+  },
+  login: {
+    path: '/login',
+    title: 'pages.login'
+  },
+  loginGuest: {
+    path: '/login/guest',
+    title: 'pages.login'
+  },
+  registration: {
+    path: '/registration',
+    title: 'pages.registration'
+  },
+  registrationGuest: {
+    path: '/registration/guest',
+    title: 'pages.registration'
+  },
+  users: {
+    path: '/users',
+    title: 'pages.users'
+  },
+  todo: {
+    path: '/todo',
+  }
+}
+
+export default routes;
